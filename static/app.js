ffe036654dec9e08a7f6c7a90f9165d9b48c83bf
h4hassanali <hxn112233@gmail.com>
2024-05-19 20:00:59 +0500

    Added Dark Mode

--- static/app.js
+++ static/app.js
@@ -1,99 +1,11 @@
-// class Chatbox {
-//     constructor() {
-//         this.args = {
-//             openButton: document.querySelector('.chatbox__button'),
-//             chatBox: document.querySelector('.chatbox__support'),
-//             sendButton: document.querySelector('.chatbox__send--footer'), // Changed to match the HTML
-//             inputField: document.querySelector('.chatbox__footer input') // Added input field selector
-//         }
-
-//         this.state = false;
-//         this.messages = [];
-//     }
-
-//     display() {
-//         const { openButton, chatBox, sendButton, inputField } = this.args;
-
-//         openButton.addEventListener('click', () => this.toggleState(chatBox))
-
-//         sendButton.addEventListener('click', () => this.onSendButton(chatBox, inputField))
-
-//         inputField.addEventListener("keyup", ({key}) => {
-//             if (key === "Enter") {
-//                 this.onSendButton(chatBox, inputField)
-//             }
-//         })
-//     }
-
-//     toggleState(chatbox) {
-//         this.state = !this.state;
-
-//         // Show or hide the box
-//         if(this.state) {
-//             chatbox.classList.add('chatbox--active')
-//         } else {
-//             chatbox.classList.remove('chatbox--active')
-//         }
-//     }
-
-//     onSendButton(chatbox, inputField) {
-//         var text1 = inputField.value.trim(); // Modified to trim whitespace
-//         if (text1 === "") {
-//             return;
-//         }
-
-//         let msg1 = { name: "User", message: text1 }
-//         this.messages.push(msg1);
-
-//         fetch('/get_response', { // Modified to match Flask route
-//             method: 'POST',
-//             body: JSON.stringify({ question: text1 }), // Modified to match Flask request format
-//             headers: {
-//               'Content-Type': 'application/json'
-//             },
-//           })
-//           .then(response => response.json())
-//           .then(data => {
-//             let msg2 = { name: "Sam", message: data.response };
-//             this.messages.push(msg2);
-//             this.updateChatText(chatbox)
-//             inputField.value = ''
-
-//         }).catch((error) => {
-//             console.error('Error:', error);
-//             this.updateChatText(chatbox)
-//             inputField.value = ''
-//           });
-//     }
-
-//     updateChatText(chatbox) {
-//         var html = '';
-//         this.messages.slice().reverse().forEach(function(item, index) {
-//             if (item.name === "Sam")
-//             {
-//                 html += '<div class="messages__item messages__item--operator">' + item.message + '</div>' // Changed class to match HTML
-//             }
-//             else
-//             {
-//                 html += '<div class="messages__item messages__item--visitor">' + item.message + '</div>' // Changed class to match HTML
-//             }
-//           });
-
-//         const chatmessage = chatbox.querySelector('.chatbox__messages');
-//         chatmessage.innerHTML = html;
-//     }
-// }
-
-
-// const chatbox = new Chatbox();
-// chatbox.display();
 class Chatbox {
     constructor() {
         this.args = {
             openButton: document.querySelector('.chatbox__button'),
             chatBox: document.querySelector('.chatbox__support'),
-            sendButton: document.querySelector('.chatbox__send--footer'), // Changed to match the HTML
-            inputField: document.querySelector('.chatbox__footer input') // Added input field selector
+            sendButton: document.querySelector('.chatbox__send--footer'),
+            inputField: document.querySelector('.chatbox__footer input'),
+            darkModeToggle: document.getElementById('darkModeToggle')
         }
 
         // Define the audio object and load the audio file
@@ -104,61 +16,73 @@ class Chatbox {
     }
 
     display() {
-        const { openButton, chatBox, sendButton, inputField } = this.args;
+        const { openButton, chatBox, sendButton, inputField, darkModeToggle } = this.args;
 
-        openButton.addEventListener('click', () => this.toggleState(chatBox))
-
-        sendButton.addEventListener('click', () => this.onSendButton(chatBox, inputField))
-
-        inputField.addEventListener("keyup", ({key}) => {
+        openButton.addEventListener('click', () => this.toggleState(chatBox));
+        sendButton.addEventListener('click', () => this.onSendButton(chatBox, inputField));
+        inputField.addEventListener("keyup", ({ key }) => {
             if (key === "Enter") {
-                this.onSendButton(chatBox, inputField)
+                this.onSendButton(chatBox, inputField);
             }
-        })
+        });
+
+        darkModeToggle.addEventListener('click', () => this.toggleDarkMode());
     }
 
     toggleState(chatbox) {
         this.state = !this.state;
 
         // Show or hide the box
-        if(this.state) {
-            chatbox.classList.add('chatbox--active')
+        if (this.state) {
+            chatbox.classList.add('chatbox--active');
         } else {
-            chatbox.classList.remove('chatbox--active')
+            chatbox.classList.remove('chatbox--active');
         }
     }
 
+    toggleDarkMode() {
+        document.body.classList.toggle('dark-mode');
+        document.querySelector('.chatbox__support').classList.toggle('dark-mode');
+        document.querySelector('.chatbox__header').classList.toggle('dark-mode');
+        document.querySelector('.chatbox__messages').classList.toggle('dark-mode');
+        document.querySelectorAll('.messages__item').forEach(item => item.classList.toggle('dark-mode'));
+        document.querySelector('.chatbox__footer').classList.toggle('dark-mode');
+        document.querySelector('.chatbox__footer input').classList.toggle('dark-mode');
+        document.querySelector('.chatbox__button button').classList.toggle('dark-mode');
+        document.querySelector('.dark-mode-toggle').classList.toggle('dark-mode');
+    }
+
     onSendButton(chatbox, inputField) {
-        var text1 = inputField.value.trim(); // Modified to trim whitespace
+        var text1 = inputField.value.trim();
         if (text1 === "") {
             return;
         }
 
-        let msg1 = { name: "User", message: text1 }
+        let msg1 = { name: "User", message: text1 };
         this.messages.push(msg1);
 
-        fetch('/get_response', { // Modified to match Flask route
+        fetch('/get_response', {
             method: 'POST',
-            body: JSON.stringify({ question: text1 }), // Modified to match Flask request format
+            body: JSON.stringify({ question: text1 }),
             headers: {
-              'Content-Type': 'application/json'
+                'Content-Type': 'application/json'
             },
-          })
-          .then(response => response.json())
-          .then(data => {
+        })
+        .then(response => response.json())
+        .then(data => {
             let msg2 = { name: "Sam", message: data.response };
             this.messages.push(msg2);
-            this.updateChatText(chatbox)
-            inputField.value = ''
+            this.updateChatText(chatbox);
+            inputField.value = '';
 
             // Play the message tone
             this.playMessageTone();
-
-        }).catch((error) => {
+        })
+        .catch((error) => {
             console.error('Error:', error);
-            this.updateChatText(chatbox)
-            inputField.value = ''
-          });
+            this.updateChatText(chatbox);
+            inputField.value = '';
+        });
     }
 
     playMessageTone() {
@@ -168,21 +92,22 @@ class Chatbox {
     updateChatText(chatbox) {
         var html = '';
         this.messages.slice().reverse().forEach(function(item, index) {
-            if (item.name === "Sam")
-            {
-                html += '<div class="messages__item messages__item--operator">' + item.message + '</div>' // Changed class to match HTML
+            if (item.name === "Sam") {
+                html += '<div class="messages__item messages__item--operator">' + item.message + '</div>';
+            } else {
+                html += '<div class="messages__item messages__item--visitor">' + item.message + '</div>';
             }
-            else
-            {
-                html += '<div class="messages__item messages__item--visitor">' + item.message + '</div>' // Changed class to match HTML
-            }
-          });
+        });
 
         const chatmessage = chatbox.querySelector('.chatbox__messages');
         chatmessage.innerHTML = html;
+
+        // Apply dark mode to new messages if dark mode is active
+        if (document.body.classList.contains('dark-mode')) {
+            chatmessage.querySelectorAll('.messages__item').forEach(item => item.classList.add('dark-mode'));
+        }
     }
 }
-
 
 const chatbox = new Chatbox();
 chatbox.display();
